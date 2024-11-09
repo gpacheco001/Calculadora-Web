38cb5bab3ee98f08660a8dafd175518a3eebe48c
gpacheco001 <guillermopacheco001@gmail.com>
2024-11-09 09:14:18 -0300

    Calculadora suma

---
+++ CalculadoraWeb/Controllers/CalculadoraController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalculadoraWeb.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        [HttpGet]
+        public IActionResult Sumar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Sumar(int numero1, int numero2)
+        {
+            int resultado = numero1 + numero2;
+            ViewBag.Resultado = resultado;
+            return View();
+        }
+    }
+}
